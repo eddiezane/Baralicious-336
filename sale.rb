ee915d8713aed54bce87934945c9e302b57a4b45
Ian Lozinski <ian.lozinski@gmail.com>
2013-11-21 21:17:57 -0500

    threshold

--- sale.rb
+++ sale.rb
@@ -11,6 +11,22 @@ class Sale
   def time_rand from = Time.local(2013, 1, 1), to = Time.now
     time = Time.at(from + rand * (to.to_f - from.to_f))
 
-    case
+    case time.day
+      #sunday
+      when 0
+
+      when 1
+        threshold = 7
+      when 2
+        threshold = 5
+      when 3
+        threshold = 7
+      when 4
+        threshold = 4
+      when 5
+        threshold = 2
+      when 6
+        threshold = 3
+    end
   end
 end
